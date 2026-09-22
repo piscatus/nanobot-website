8adc837afa66a6d9e3f7c4fa73f597a56159df8d
piscatus <90541885+piscatus@users.noreply.github.com>
2026-09-22 14:26:47 -0400

    Update copyright year in Footer component

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -6,7 +6,7 @@ export default function Footer() {
       <div className="border-t border-white border-opacity-10 flex justify-between py-6">
         <div className="grid gap-2">
           <span className="flex items-end">
-            © 2023 - 2025 Nanobot - All rights reserved.
+            © 2023 - 2026 Nanobot - All rights reserved.
           </span>
           <div className="text-gray flex gap-3">
             <FooterLink href="/terms">Terms of Service</FooterLink>
